b135c4b918ca78a09d7b3e32e2d79fd4203e1fec
Julian <julianmuriel510@gmail.com>
2026-03-13 19:05:58 -0300

    Comenzando proyecto

---
+++ src/operaciones.js
@@ -0,0 +1,18 @@
+//declaro mis funciones
+
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const multi = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+
+//dos formas de importar y exportar modulos:
+//Common JS
+//ES Module(llega en el año 2015)
+
+module.exports = {
+    suma,
+    resta,
+    multi,
+    division,
+}
